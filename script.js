52114ce0e321458c6606f6a51a3fcfb1c1db113b
Philipp Binder <88280928+philippbinder@users.noreply.github.com>
2022-03-15 00:19:36 +0100

    Restructure and add close modal functionality for overlay

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-'strict mode';
+'use strict';
+
+// state variables
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+// open modal window function
+const openModalFunction = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+// close modal window function
+const closeModalFunction = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  console.log(btnShowModal[i].textContent);
+}
+
+// loop over all elements in the btnShowModal const -> addEventListener to remove the hidden class
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModalFunction);
+}
+
+// closeModalFunction() would immediately as soon as JS executes this line instead of calling the function on click
+btnCloseModal.addEventListener('click', closeModalFunction);
+overlay.addEventListener('click', closeModalFunction);
